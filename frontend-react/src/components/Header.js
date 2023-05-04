250e05934fd8df5d836448a25708e08fbb3de728
Crystal Broderick <broderick.crystal@gmail.com>
2023-05-04 15:47:49 -0700

    update header style

--- frontend-react/src/components/Header.js
+++ frontend-react/src/components/Header.js
@@ -1,11 +1,21 @@
-
-function Header() {
+import { Container, Image, Menu } from "semantic-ui-react"
+function Header({ title }) {
+  const menuStyle = {
+    backgroundColor: "#CC2522",
+    border: "1px solid #ddd",
+    boxShadow: "0px 3px 5px rgba(0, 0, 0, 0.2)",
+  }
   return (
-    <header>
-      <img className="logo" src={`${window.location.origin}/Hotdog_logo.jpg`} />
-      <h1>HOTDOG CARTS</h1>
-    </header>
-      
+    <Menu borderless style={menuStyle}>
+      <Container>
+        <Menu.Item>
+          <Image size="tiny" className="logo" src="/Hotdog_logo.jpg" />
+        </Menu.Item>
+        <Menu.Item header>
+          <h1 style={{ color: "#fff" }}>{title}</h1>
+        </Menu.Item>
+      </Container>
+    </Menu>
   )
 }
 
