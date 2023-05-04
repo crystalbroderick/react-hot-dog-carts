12a1195109c227f9f7c5576fb4c8c4a2e17a6a4c
Crystal Broderick <broderick.crystal@gmail.com>
2023-05-04 15:59:14 -0700

    info window styles

--- frontend-react/src/components/Map.js
+++ frontend-react/src/components/Map.js
@@ -1,29 +1,32 @@
-import React from "react";
-import { GoogleMap, LoadScript, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
-import apiKey from "../googleMapsApiKey.json";
-
+import React from "react"
 import {
-    Link
-  } from "react-router-dom";
-  
+  GoogleMap,
+  LoadScript,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api"
+import apiKey from "../googleMapsApiKey.json"
+import { Container, Button } from "semantic-ui-react"
+import { Link } from "react-router-dom"
 
 const mapStyles = {
-    height: "90vh",
-    width: "90%",
-    margin: "1em auto"
-};
+  height: "80vh",
+  // marginTop: "4em",
+}
 
-const onLoad = marker => {
-    console.log('marker: ', marker)
+const onLoad = (marker) => {
+  console.log("marker: ", marker)
 }
 
 const defaultCenter = {
-    lat: 47.65629189632677, lng: -122.32059609201939
+  lat: 47.65629189632677,
+  lng: -122.32059609201939,
 }
 
 const options = {
-    disableDefaultUI: true,
-    zoomControl: true
+  disableDefaultUI: true,
+  zoomControl: true,
 }
 
 /* const mapRef = React.useRef();
@@ -31,104 +34,110 @@ const options = {
         mapRef.current = map;
     }, []); */
 
-
 class MapContainer extends React.Component {
-    constructor(props) {
-        super(props); 
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            carts: [],
-            selected: null,
-            setSelected: null
-        }
+    this.state = {
+      carts: [],
+      selected: null,
+      setSelected: null,
     }
-    
+  }
 
-    componentDidMount (){
-        let test = fetch("http://localhost:3000/users/map")
-        .then((results) => {
-            return results.json();
-        }).then((myJson) => {
-            console.log("FetchResolved", myJson);
-            this.setState({
-                carts: myJson
-            })
-            //console.log(this.state.carts[0].Cart_Id);
-            
-        })}
-     
-        resetSelected() {
-            this.setState({selected: { } })
-        }
+  componentDidMount() {
+    let test = fetch("http://localhost:3000/users/map")
+      .then((results) => {
+        return results.json()
+      })
+      .then((myJson) => {
+        console.log("FetchResolved", myJson)
+        this.setState({
+          carts: myJson,
+        })
+        //console.log(this.state.carts[0].Cart_Id);
+      })
+  }
 
-render() { 
-    
-    console.log(this.state.selected);
-    const cartMarker = this.state.carts.map(cart => {
-        
-        return <Marker
-            key={cart.Cart_Id}
-            position={{
-                lat: parseFloat(cart.Latitude),
-                lng: parseFloat(cart.Longitude)    
-            }} 
-            cart={cart} 
-            onClick={() => {
-                this.setState({ selected: [cart]})
-            }
-            } 
-            />;
-            
-    });
+  resetSelected() {
+    this.setState({ selected: {} })
+  }
+
+  render() {
+    console.log(this.state.selected)
+    const cartMarker = this.state.carts.map((cart) => {
+      return (
+        <Marker
+          key={cart.Cart_Id}
+          position={{
+            lat: parseFloat(cart.Latitude),
+            lng: parseFloat(cart.Longitude),
+          }}
+          cart={cart}
+          onClick={() => {
+            this.setState({ selected: [cart] })
+          }}
+        />
+      )
+    })
 
     const info = this.state.selected ? (
-        //console.log("selected: " + this.state.selected[0].Cart_Name),
-        <InfoWindow
-        position = {
-            {lat: parseFloat(this.state.selected[0].Latitude), lng: parseFloat(this.state.selected[0].Longitude)}
-        }
-        onCloseClick={() => {this.setState({ selected: null})}}
-         >
+      //console.log("selected: " + this.state.selected[0].Cart_Name),
+      <InfoWindow
+        position={{
+          lat: parseFloat(this.state.selected[0].Latitude),
+          lng: parseFloat(this.state.selected[0].Longitude),
+        }}
+        onCloseClick={() => {
+          this.setState({ selected: null })
+        }}
+      >
         <div className="infowindow">
-        
-            <h2>{this.state.selected[0].Cart_Name}</h2>
-            <h5>Currently at:</h5>
-                <p>{this.state.selected[0].Cart_Location}</p>
-            <h5>Current Menu options:</h5>
-                <p>{this.state.selected[0].Items}</p>
-             {/* </div><button> Order Here! </button>  */}
-            <div>
-            <Link to ="/menu"><button>Order Here!</button></Link>
-                </div>
-            
-            
+          <h2>{this.state.selected[0].Cart_Name}</h2>
+          <h5>Currently at:</h5>
+          <p>{this.state.selected[0].Cart_Location}</p>
+          <h5>Current Menu options:</h5>
 
+          <p>{this.state.selected[0].Items}</p>
+          {/* </div><button> Order Here! </button>  */}
+          <Link to={`/menu/${this.state.selected[0].Cart_Id}`}>
+            <div class="ui animated button yellow" tabIndex="0">
+              <div class="visible content">Order Ahead</div>
+              <div class="hidden content">
+                <i className="right arrow icon"></i>
+              </div>
+            </div>{" "}
+          </Link>
         </div>
-    </InfoWindow>): null;
-    
-return (
-    
-<div>
-<LoadScript googleMapsApiKey={apiKey.key}> 
-    <GoogleMap
-        mapContainerStyle={mapStyles}
-        center={defaultCenter}
-        zoom={13}
-        options={options}
-        
-        carts={this.state.carts}
-        >
-            <h2 className="maph2">Hot Dogs!<span role="img" aria-label="hotdog">🌭</span></h2>
-            {cartMarker }
-            {info }
-            
+      </InfoWindow>
+    ) : null
+
+    return (
+      <>
+        {" "}
+        <LoadScript googleMapsApiKey={apiKey.key}>
+          <GoogleMap
+            mapContainerStyle={mapStyles}
+            center={defaultCenter}
+            zoom={13}
+            options={options}
+            carts={this.state.carts}
+          >
+            <h2 className="maph2">
+              Hot Dogs!
+              <span role="img" aria-label="hotdog">
+                🌭
+              </span>
+            </h2>
+            {cartMarker}
+            {info}
+
             {/* <Marker onLoad={onLoad} position={defaultCenter} /> */}
-    </GoogleMap>
-    </LoadScript>
-</div>)
+          </GoogleMap>
+        </LoadScript>
+      </>
+    )
+  }
+}
 
-            }
-        }
-    
-export default MapContainer;
-
+export default MapContainer
